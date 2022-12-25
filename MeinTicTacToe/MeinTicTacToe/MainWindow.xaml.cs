81cfe3b50ea44ffb14c537303b78073c2a2b3368
zulander2109 <115042712+zulander2109@users.noreply.github.com>
2022-12-25 23:10:26 +0100

    Add files via upload

---
+++ MeinTicTacToe/MeinTicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeinTicTacToe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kaestchen_0_0_Click(object sender, RoutedEventArgs e)
+        {
+            Kaestchen_0_0.Background = Brushes.Red;
+            Kaestchen_0_0.Foreground = Brushes.Black;
+        }
+    }
+}
