4f71d6fbc4256617c718c53c1860a37bdbe4369b
Admir Zulic <zulicadmir@outlook.com>
2022-12-26 17:41:35 +0100

    alle kästchen sind befüllt

--- MeinTicTacToe/MainWindow.xaml.cs
+++ MeinTicTacToe/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,12 +27,250 @@ namespace MeinTicTacToe
             InitializeComponent();
         }
 
+
+        int spielerX=0;
+                
         private void Kaestchen_0_0_Click(object sender, RoutedEventArgs e)
         {
-            var urspuenglicheFarbe = kaestchen_0_0.Foreground;
-            kaestchen_0_0.Foreground = kaestchen_0_0.Background;
-            kaestchen_0_0.Background = urspuenglicheFarbe;
+            Button geklickt = (Button)sender;
+            NewMethod();
             
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_0_1_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_0_2_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_1_0_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_1_1_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_1_2_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_2_0_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_2_1_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
+        }
+
+        private void kaestchen_2_2_Click(object sender, RoutedEventArgs e)
+        {
+            Button geklickt = (Button)sender;
+            NewMethod();
+
+
+            void NewMethod()
+            {
+                if (spielerX % 2 == 0)
+                {
+                    geklickt.Content = "X";
+                    var urspuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = urspuenglicheFarbe;
+                }
+                else
+                {
+                    geklickt.Content = "O";
+                    var ursuenglicheFarbe = geklickt.Foreground;
+                    geklickt.Foreground = geklickt.Background;
+                    geklickt.Background = ursuenglicheFarbe;
+
+                }
+                spielerX += 1;
+            }
         }
     }
 }
